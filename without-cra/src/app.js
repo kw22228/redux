0c2b6257b6a6cc23c48bdfe4e3642b0179383ec1
kw22228 <hot5521@naver.com>
2022-04-29 17:44:28 +0900

    delete

--- without-cra/src/app.js
+++ without-cra/src/app.js
@@ -3,7 +3,7 @@ import { render, createElement } from './react';
 
 const vdom = (
     <div id="wrap" style="color:orange">
-        첫번째 엘리먼트
+        첫번째 엘리먼트!!
         <h1 style="color:blue">
             VirtualDom에 대해서!!!
             <h6>hello</h6>
