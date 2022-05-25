a98e0ccc66a56bb2e0743ec29390e22b0c59617d
김재원 <kw2282222@gmail.com>
2022-05-26 00:04:05 +0900

    add: store

--- coders/practice-redux/src/store/store.js
+++ coders/practice-redux/src/store/store.js
@@ -1,10 +1,12 @@
-import { TodoReducer } from './reducers';
+import { todoReducer } from './reducers/';
 import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
 
 const reducers = {
-    todos: TodoReducer,
+    todos: todoReducer.reducer,
 };
+const rootReducer = combineReducers(reducers);
 
 export const store = configureStore({
-    reducer: reducers,
+    reducer: rootReducer,
 });
