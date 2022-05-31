5bd546648bce8d9d9acb2d425581a6f928fc0e56
kw22228 <hot5521@naver.com>
2022-05-31 17:47:29 +0900

    add: middleware

--- redux/redux-middleware/app.js
+++ redux/redux-middleware/app.js
@@ -2,18 +2,21 @@ import { createStore } from './store/redux.js';
 import * as Actions from './store/actions/actions.js';
 import reducer from './store/reducer/reducer.js';
 import * as middlewares from './store/middlewares/index.js';
+import { asyncJobs } from './store/job/asyncJobs.js';
 
-const store = createStore(reducer, [middlewares.logger]);
+const store = createStore(reducer, [middlewares.logger, middlewares.asyncRouter(asyncJobs)]);
 
 const counterDisplay = document.querySelector('#counter');
+const loadingMessage = document.querySelector('#loading');
 const btnIncrease = document.querySelector('#btn-increase');
 const btnAsyncIncrease = document.querySelector('#btn-async-increase');
 const btnDecrease = document.querySelector('#btn-decrease');
 const btnReset = document.querySelector('#btn-reset');
 
 store.subscribe(function () {
-    const { counter } = store.getState();
+    const { counter, request } = store.getState();
 
+    loadingMessage.style.visibility = request ? 'visible' : 'hidden';
     counterDisplay.textContent = counter;
 });
 
